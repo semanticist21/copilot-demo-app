3e0e9d7dca39e3975aa2c40dbcd088f82a4b4445
jiwon <semanticist0@gmail.com>
2024-01-28 22:12:59 +0900

    🎨

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
@@ -17,14 +15,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
